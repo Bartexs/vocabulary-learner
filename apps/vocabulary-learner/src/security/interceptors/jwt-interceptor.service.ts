65ed1e3d72594369ed21923ac8001e3f1133f0d9
Bartexs <barttosz.witkowski@gmail.com>
2025-07-28 22:51:17 +0200

    Logout on error

--- apps/vocabulary-learner/src/security/interceptors/jwt-interceptor.service.ts
+++ apps/vocabulary-learner/src/security/interceptors/jwt-interceptor.service.ts
@@ -1,11 +1,25 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 export const JwtInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
-  if (token) {
-    req = req.clone({
-      setHeaders: { Authorization: `Bearer ${token}` }
-    });
-  }
-  return next(req);
+
+  const authReq = token
+    ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
+    : req;
+
+  const router = inject(Router);
+
+  return next(authReq).pipe(
+    catchError(error => {
+      if (error.status != 0) {
+        localStorage.removeItem('token'); // optional
+        router.navigate(['/login']); // or /register
+      }
+
+      return throwError(() => error);
+    })
+  );
 };
