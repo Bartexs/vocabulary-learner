1af65d3a9b58337d5add48814a12f732883ca954
Bartexs <barttosz.witkowski@gmail.com>
2025-10-09 23:08:24 +0200

    ECS-92 ui: Develop Homepage and close ECS-48-homepage-development

--- apps/vocabulary-learner/src/app/features/home/home.component.ts
+++ apps/vocabulary-learner/src/app/features/home/home.component.ts
@@ -5,6 +5,7 @@ import { Folder } from '../../core/models/folder';
 import { DateUtilsService } from '../../core/services/date-utils.service';
 import { FlashcardService } from '../../core/services/flashcard.service';
 import { LessonService } from '../../core/services/lesson.service';
+import { RouterLink } from "@angular/router";
 
 interface FolderWithFlashcards {
   folder: Folder,
@@ -15,85 +16,11 @@ interface FolderWithFlashcards {
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule ],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 
 export class HomeComponent {
-  examEnabled = false;
-  practiceEnabled = true;
-  repetitionFlashcardList: Flashcard[] | undefined;
-  newFlashcardList: Flashcard[] | undefined;
-  folderList: FolderWithFlashcards[] | undefined;
-
-  constructor(
-    private flashcardService: FlashcardService,
-    private lessonService: LessonService,
-    private dateUtils: DateUtilsService
-  ) {
-
-  }
-
-  // ngOnInit() {
-    // this.getRepetitionMaterial();
-    // this.getNewFlashcardList();
-    // this.getFolderList();
-  // }
-
-  // getFolderList() {
-  //   let mergedArray;
-  //   const folderListWithFlashcards: FolderWithFlashcards[] = [];
-
-  //   if(this.repetitionFlashcardList && this.newFlashcardList) {
-  //     mergedArray = [ ...this.repetitionFlashcardList, ...this.newFlashcardList ]
-  //   };
-
-  //   mergedArray?.forEach((element) => {
-  //     // const folderFound = this.getFolderByFlashcard(element);
- 
-  //     // const folderWithFlashcard = folderListWithFlashcards.find((item) => item.folder.id === folderFound.id);
-    
-  //     if (folderWithFlashcard) {
-  //       folderWithFlashcard.flashcardList.push(element);
-  //     } else {
-  //       const newList = [];
-  //       const repetitionList = [];
-
-  //       const isNewMaterial = this.flashcardService.isFlashcardNew(element);
-
-  //       if(isNewMaterial) {
-  //         newList.push(element) 
-  //       } else {
-  //         repetitionList.push(element);
-  //       }
-
-  //       folderListWithFlashcards.push({
-  //         folder: folderFound,
-  //         flashcardList: [element],
-  //         newMaterial: [],
-  //         repetitionMaterial: []
-  //       });
-  //     }
-  //   });
-
-  //   this.folderList = folderListWithFlashcards;
-  // }
-
-  // // getFolderByFlashcard(flashcard: Flashcard): Folder {
-  // //   const lesson = this.lessonService.getLessonById(flashcard.lessonId);
-  // //   return this.folderService.getFolderById(lesson!.folderId);
-  // // }
-
-  // getRepetitionMaterial() {
-  //   this.repetitionFlashcardList = this.flashcardService.getRepetitionMaterial();
-  // }
-
-  // getNewFlashcardList() {
-  //   this.newFlashcardList = this.flashcardService.getNewFlashcards();
-  // }
-
-  clearLocalStorage() {
-    localStorage.clear();
-  }
+  isPracticeDisabled = false;
 }
