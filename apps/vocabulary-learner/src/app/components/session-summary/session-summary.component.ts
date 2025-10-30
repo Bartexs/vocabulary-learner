4caeb207c27a1cf7e7e59e186f32d12a4b63c127
Bartexs <barttosz.witkowski@gmail.com>
2025-10-30 17:52:09 +0100

    Changed href to routerLink, added 404 page to work on ghpages

--- apps/vocabulary-learner/src/app/components/session-summary/session-summary.component.ts
+++ apps/vocabulary-learner/src/app/components/session-summary/session-summary.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SessionSummaryService } from './session-summary.service';
 import { ExerciseSummary } from '../../core/models/exercise-Summary';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-session-summary',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './session-summary.component.html',
   styleUrl: './session-summary.component.css',
 })
