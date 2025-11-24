11d5e989ecb8735cd78410730e6347a175e4316d
Bartexs <barttosz.witkowski@gmail.com>
2025-11-24 20:18:25 +0100

    ECS-130 User custom exercises order

--- apps/vocabulary-learner/src/app/features/training-modes/practice/resources-configurators/exercise-selector/exercise-selector.component.ts
+++ apps/vocabulary-learner/src/app/features/training-modes/practice/resources-configurators/exercise-selector/exercise-selector.component.ts
@@ -1,47 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { ExerciseType, getExercises, getExercisesByNames } from '../../../../../core/models/exercise';
-import { LearningSessionConfigService } from '@vocabulary-learner/shared/services/learning-session-config-service/learning-session-config.service';
+import { ExerciseType, getExercises } from '../../../../../core/models/exercise';
+import { LearningSessionConfigService } from '../../../../../shared/services/learning-session-config-service/learning-session-config.service';
+import { CdkDrag, CdkDragDrop, CdkDropList } from '@angular/cdk/drag-drop';
+import { MatIcon } from "@angular/material/icon";
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-exercise-selector',
-  imports: [CommonModule, MatCheckboxModule],
+  standalone: true,
+  imports: [CommonModule, MatCheckboxModule, CdkDropList, CdkDrag, MatIcon, ReactiveFormsModule],
   templateUrl: './exercise-selector.component.html',
-  styleUrl: './exercise-selector.component.css',
+  styleUrls: ['./exercise-selector.component.css'],
 })
 export class ExerciseSelectorComponent implements OnInit {
-  exerciseList: ExerciseType[] = [];
-  selectedExercises: string[] = [];
+  exerciseContainer: (ExerciseType & { control: FormControl })[] = [];
+
+  constructor(private sessionConfigService: LearningSessionConfigService) {}
 
   ngOnInit() {
-    this.exerciseList = getExercises();
+    this.exerciseContainer = getExercises().map(ex => ({
+      ...ex,
+      control: new FormControl(false)
+    }));
+
+    // Subscribe individually to each control
+    this.exerciseContainer.forEach(item => {
+      item.control.valueChanges.subscribe(() => {
+        this.reorderItems();
+      });
+    });
   }
 
-  constructor(
-    private sessionConfigService: LearningSessionConfigService,
-  ) {
+  // Reorder items: checked items stay on top, unchecked at the bottom
+  reorderItems() {
+    const checkedItems = this.exerciseContainer.filter(i => i.control.value);
+    const uncheckedItems = this.exerciseContainer.filter(i => !i.control.value);
 
+    this.exerciseContainer = [...checkedItems, ...uncheckedItems];
+
+    // Update service with currently selected exercises
+    this.updatePracticeModeConfig(checkedItems);
   }
 
-  toggleSelection(exercise: string, isChecked: boolean): void {
+  drop(event: CdkDragDrop<(ExerciseType & { control: FormControl })[]>) {
+    if (event.previousIndex !== event.currentIndex) {
+      // Swap items in container
+      [this.exerciseContainer[event.previousIndex], this.exerciseContainer[event.currentIndex]] =
+        [this.exerciseContainer[event.currentIndex], this.exerciseContainer[event.previousIndex]];
 
-    if (isChecked) {
-      // add checked exercise to selected exercise list
-      this.selectedExercises.push(exercise);
-    } else {
-      // remove unchecked exercise from selected exercise list
-      this.selectedExercises = this.selectedExercises.filter(e => e !== exercise);
+      // Automatically check the moved item
+      const movedItem = this.exerciseContainer[event.currentIndex];
+      movedItem.control.setValue(true, { emitEvent: false });
     }
 
-    // search for exercise objects by exercise name (convert from string to ExerciseType)
-    const updatedExerciseList = getExercisesByNames(this.selectedExercises);
-
-    // update practice-mode component on changes
-    this.updatePracticeModeConfig(updatedExerciseList);
+    // Reorder after drop to enforce checked on top, unchecked at bottom
+    this.reorderItems();
   }
 
-  updatePracticeModeConfig(exerciseList: ExerciseType[]) {
-    this.sessionConfigService.updateConfig({exerciseList});
+  updatePracticeModeConfig(selected: ExerciseType[]) {
+    console.log(selected);
+    this.sessionConfigService.updateConfig({ exerciseList: selected });
   }
 }
